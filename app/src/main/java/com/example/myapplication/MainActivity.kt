8f62db0fd7f46289ff47b4f2e22a53d5a8206f37
nn <nn@em.com>
2024-04-11 23:23:15 +0700

    ....

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import android.database.Cursor
 import android.os.Bundle
 import android.provider.MediaStore
 import android.webkit.WebView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -25,36 +26,53 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        println("start...")
+        println("start...1")
         super.onCreate(savedInstanceState)
 
         //这里管理起来gui.xml文件。。
         setContentView(R.layout.activity_main)
 
-        val webView1: WebView =findViewById(R.id.webView1)
+        val webView1: WebView = findViewById(R.id.webView1)
         //是否与JavaScript交互
         webView1.settings.javaScriptEnabled = true
 //是否打开Dom本地存储
         webView1.settings.domStorageEnabled = true
         webView1.settings.setAllowFileAccess(true)
-        webView1.loadUrl("file:///android_asset/idx.htm")
-      //  webView1.loadUrl("https://www.baidu.com")
+        //   webView1.loadUrl("file:///android_asset/idx.htm")
+        webView1.loadUrl("https://www.baidu.com")
 
 
+        val TextView1: TextView = findViewById(R.id.txtOne)
+        TextView1.setText("aaaaaaaaaa")
         //主Actvitiy里面启动Service        1
-       // startService(  Intent(this, ImgService.class))
+        // startService(  Intent(this, ImgService.class))
 
 
         val READ_EXTERNAL_STORAGE_PERMISSION_CODE = 101
-      //   检查是否已经获取了读取相册权限
-        if (ContextCompat.checkSelfPermission(this,  android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                              != PackageManager.PERMISSION_GRANTED)
-        {
-            val prmss = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        //   检查是否已经获取了读取相册权限
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            //android.Manifest.permission.READ_EXTERNAL_STORAGE
+            var prmss = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
             // 申请读取相册权限
-            ActivityCompat.requestPermissions(this,
+            ActivityCompat.requestPermissions(
+                this,
                 prmss,
-                READ_EXTERNAL_STORAGE_PERMISSION_CODE);
+                READ_EXTERNAL_STORAGE_PERMISSION_CODE
+            );
+
+            prmss = arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES)
+            ActivityCompat.requestPermissions(
+                this,
+                prmss,
+                READ_EXTERNAL_STORAGE_PERMISSION_CODE
+            );
+
+
         } else {
             // 已经获取了读取相册权限，可以进行后续操作
             // ...
@@ -65,18 +83,18 @@ class MainActivity : ComponentActivity() {
         // 空数组
 //        var projection = emptyArray<String>()
 //         projection = { MediaStore.Images.Media.DATA };
-
+        //projection=[_data]
         val projection = arrayOf(MediaStore.Images.Media.DATA)
         var sortOrder = MediaStore.Images.Media.DATE_MODIFIED + " DESC";
 
 // 查询媒体数据库，获取所有图片的路径
-        var   cursor = getContentResolver().query(
-          MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-          projection,
-          null,
-          null,
-          sortOrder
-      );
+        var cursor = getContentResolver().query(
+            MediaStore.Images.Media.INTERNAL_CONTENT_URI,
+            projection,
+            null,
+            null,
+            sortOrder
+        );
 
         if (cursor != null) {
             while (cursor.moveToNext()) {
@@ -85,8 +103,7 @@ class MainActivity : ComponentActivity() {
                     cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
 
                 // 对图片路径进行处理，如添加到列表中等
-                println("imagePath=》"+imagePath)
-
+                println("imagePath=》" + imagePath)
 
 
                 // ...
@@ -97,16 +114,14 @@ class MainActivity : ComponentActivity() {
         }
 
 
-
-
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name! 121313213",
-            modifier = modifier
+        text = "Hello $name! 121313213",
+        modifier = modifier
     )
 }
 
