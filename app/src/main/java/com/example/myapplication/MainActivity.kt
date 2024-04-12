0f890cdcb6645e7468b6db1cdf8d44e0960ddea9
nn <nn@em.com>
2024-04-12 18:19:47 +0700

    file auth  adk>30

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.myapplication
 
+//import kotlinx.coroutines.flow.internal.NoOpContinuation.context
+import android.Manifest
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.Manifest.permission.READ_MEDIA_VIDEO
+import android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.database.Cursor
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.provider.Settings
 import android.webkit.WebView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.Nullable
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,6 +29,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import java.io.File
+import java.io.OutputStream
+import java.io.PrintWriter
+import java.io.Writer
+
 
 class MainActivity : ComponentActivity() {
 
@@ -43,7 +57,8 @@ class MainActivity : ComponentActivity() {
 
 
         val TextView1: TextView = findViewById(R.id.txtOne)
-        TextView1.setText("aaaaaaaaaa")
+        val text = TextView1.text.toString()
+        TextView1.setText(TextView1.text.toString() +"aaaaaaaaaa")
         //主Actvitiy里面启动Service        1
         // startService(  Intent(this, ImgService.class))
 
@@ -57,6 +72,7 @@ class MainActivity : ComponentActivity() {
             != PackageManager.PERMISSION_GRANTED
         ) {
             //android.Manifest.permission.READ_EXTERNAL_STORAGE
+           //     ActivityCompat.requestPermissions  android.Manifest.permission.WRITE_EXTERNAL_STORAGE
             var prmss = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
             // 申请读取相册权限
             ActivityCompat.requestPermissions(
@@ -72,12 +88,43 @@ class MainActivity : ComponentActivity() {
                 READ_EXTERNAL_STORAGE_PERMISSION_CODE
             );
 
+          var  prmss2 = arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+           ActivityCompat.requestPermissions(this,  prmss2,1 );
+
+            // 检查是否已经有了权限  PackageManager.PERMISSION_GRANTED =0
+            val checkSelfPermission = ContextCompat.checkSelfPermission(  this,  android.Manifest.permission.WRITE_EXTERNAL_STORAGE        )
+            if (checkSelfPermission    == PackageManager.PERMISSION_GRANTED) {
+              println("alread have prms")
+            }else
+            {
+               // checkSelfPermission==- 1
+                println("not  have prms")
+            }
+
+
 
         } else {
             // 已经获取了读取相册权限，可以进行后续操作
             // ...
         }
 
+        var  prmss2 = arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        ActivityCompat.requestPermissions(this,  prmss2,1 );
+
+        val directory = File("/storage/emulated/0/DCIM/")
+        if (directory.isDirectory) {
+            val listFiles = directory.listFiles()
+       var LSTFILEsIZE=     listFiles.size;  //==0
+            listFiles.forEach {
+                if (it.isFile) {
+                    println("File: ${it.name}")
+                    TextView1.setText(TextView1.text.toString() +" pth=>${it.name}")
+                } else if (it.isDirectory) {
+                    println("Directory: ${it.name}")
+                    TextView1.setText(TextView1.text.toString() +" pth=>${it.name}")
+                }
+            }
+        }
 
 // 设置查询的条件
         // 空数组
@@ -88,8 +135,10 @@ class MainActivity : ComponentActivity() {
         var sortOrder = MediaStore.Images.Media.DATE_MODIFIED + " DESC";
 
 // 查询媒体数据库，获取所有图片的路径
+        // INTERNAL_CONTENT_URI
+     //   MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         var cursor = getContentResolver().query(
-            MediaStore.Images.Media.INTERNAL_CONTENT_URI,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
             projection,
             null,
             null,
@@ -104,7 +153,7 @@ class MainActivity : ComponentActivity() {
 
                 // 对图片路径进行处理，如添加到列表中等
                 println("imagePath=》" + imagePath)
-
+                TextView1.setText(TextView1.text.toString() +" newpath=>"+imagePath)
 
                 // ...
             }
@@ -114,9 +163,156 @@ class MainActivity : ComponentActivity() {
         }
 
 
+        // Permission request logic
+        val requestPermissions = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
+            // Handle permission requests results
+            // See the permission example in the Android platform samples: https://github.com/android/platform-samples
+        }
+
+
+        //not work
+        var bldVerSdkInt=Build.VERSION.SDK_INT;  //34
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            //here
+            requestPermissions.launch(arrayOf(Manifest.permission.WRITE_SETTINGS  ,Manifest.permission.WRITE_EXTERNAL_STORAGE,  WRITE_EXTERNAL_STORAGE,READ_MEDIA_IMAGES, READ_MEDIA_VIDEO, READ_MEDIA_VISUAL_USER_SELECTED))
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissions.launch(arrayOf(READ_MEDIA_IMAGES, READ_MEDIA_VIDEO))
+        } else {
+            requestPermissions.launch(arrayOf(READ_EXTERNAL_STORAGE))
+        }
+
+
+       // not effec for wrt file
+        ActivityCompat.requestPermissions(this,  arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE,READ_EXTERNAL_STORAGE,android.Manifest.permission.READ_MEDIA_IMAGES,READ_MEDIA_VISUAL_USER_SELECTED),1 );
+
+
+
+        ///req file prms
+
+     //   ttt(this);
+        req_file_prm(this, ::wrtFile );
+
+    // this.context   context.getPackageName()
+        // this.
+       // this.packageName
+        val uri = Uri.parse("package:"+getPackageName())  // this.getPackageName
+
+
+
+      var prmsRzt=  ContextCompat.checkSelfPermission(     this,READ_EXTERNAL_STORAGE)
+        var prmsRzt2=  ContextCompat.checkSelfPermission(     this, WRITE_EXTERNAL_STORAGE)
+        var prmsRzt3=  ContextCompat.checkSelfPermission(     this, READ_MEDIA_IMAGES)
+
+
+       //             wrtFile()
     }
+
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, @Nullable data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == REQUEST_CODE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//
+//        }
+
+        if (Environment.isExternalStorageManager()) {
+            wrtFile();
+        } else {
+          //  ToastUtils.show("存储权限获取失败")
+        }
+    }
+
+    private fun ttt(mainActivity: MainActivity) {
+
+    }
+
+
 }
 
+
+
+fun req_file_prm(thisForm:MainActivity, writeFileFun2025:()->Unit) {
+
+//    startActivity(
+//        Intent(
+//            Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+//            uri
+//        )
+//    )
+
+   // Build.VERSION.SDK_INT==34   Build.VERSION_CODES.R==30
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        // 先判断有没有权限
+        if (Environment.isExternalStorageManager()) {
+            writeFileFun2025()
+        } else {
+            var action11=Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION;
+            val intent = Intent(action11)
+            intent.setData(Uri.parse("package:" + thisForm.getPackageName()))
+           // intent.setAction(action11)
+            thisForm.startActivityForResult(intent, 111)
+        }
+    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        // 先判断有没有权限
+        if (ActivityCompat.checkSelfPermission(
+                thisForm,
+                READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(
+                thisForm,
+                WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            writeFileFun2025()
+        } else {
+            ActivityCompat.requestPermissions(
+                thisForm,
+                arrayOf<String>(READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE),
+                1
+            )
+        }
+    } else {
+        writeFileFun2025()
+    }
+}
+fun  wrtFile()
+{
+
+
+
+    //  Environment.getExternalStorageDirectory==storage/emulate/0/
+    val file = File(Environment.getExternalStorageDirectory().toString()+"/myfile.txt")
+    file.createNewFile()
+    file.writeText("This will be written to the file!")
+    println("ok")
+//    val filename = "lg2025.txt"
+//    val string = "Hello, World!呵呵呵哈哈哈"
+//    val outputStream: FileOutputStream
+//
+//
+//      outputStream = openFileOutput(filename, Context.MODE_PRIVATE)
+//        outputStream.write(string.toByteArray())
+//        outputStream.close()
+}
+fun writeFile() {
+   val currentDir = System.getProperty("user.dir") + "\\out11"
+    val file = File(  currentDir,"lg2024.txt")
+    file.writeText("呵呵呵哈哈哈")
+    println(file.readText())
+
+//    file.writeBytes(byteArrayOf(12, 56, 83, 57))
+//    println(file.readText())
+//
+//    //追加方式写入字节或字符
+//    file.appendBytes(byteArrayOf(93, 85, 74, 93))
+//    file.appendText("吼啊")
+//    println(file.readText())
+
+    //直接使用writer和outputstream
+    val writer: Writer = file.writer()
+    val outputStream: OutputStream = file.outputStream()
+    val printWriter: PrintWriter = file.printWriter()
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
